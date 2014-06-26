817b2314e3ccf2e82ada9ee607770b4d5b6aa92c
brianmsmith <brianmsmith777@gmail.com>
2014-06-25 23:06:54 -0500

    IntrotoCSharp

---
+++ BrianConsoleAPp/BrianConsoleAPp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrianConsoleAPp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputReadLine;
+            Console.WriteLine("Hello World!!");
+            inputReadLine = Console.ReadLine();
+            Console.WriteLine(inputReadLine);
+            Console.ReadLine();
+        }
+    }
+}
